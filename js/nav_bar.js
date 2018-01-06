e1db6315072ded4056261127f2f671cd2eef5259
Yi Hsiao <hsiaoyi0504@gmail.com>
2018-01-06 16:35:49 -0500

    fix goURL

--- js/nav_bar.js
+++ js/nav_bar.js
@@ -1,14 +1,15 @@
 function goURL(){
-	var url = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
+	var url = window.location.protocol + "//" + window.location.host + window.location.pathname;
 	var pos = url.indexOf(".html");
 	var flag = url.indexOf("_zh");
-	var newURL = url;
-	if(pos > -1){
-		if(flag > -1)
-			newURL = url.substr(0, flag)+url.substr(pos);		
-		else
-			newURL = url.substr(0, pos)+"_zh"+url.substr(pos);	
+	if(window.location.pathname === '/') {
+		newURL = window.location.protocol + "//" + window.location.host + '/index_zh.html';
+	} else if (pos > -1) {
+		if(flag > -1) {
+			newURL = url.substr(0, flag)+url.substr(pos);
+		} else {
+			newURL = url.substr(0, pos)+"_zh"+url.substr(pos);
+		}
 	}
 	window.location.href = newURL;
-	
 }
