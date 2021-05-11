dfe5ba49f8d75c6c0fb01325ab2819eff84cb67f
Stuart Yang <adp871111@gmail.com>
2021-05-11 19:33:31 +0800

    update webpage structure with "?content=" flag

--- js/nav_bar.js
+++ js/nav_bar.js
@@ -1,20 +1,21 @@
-var theContent;
-function goURL(){
+function goURL() {
 	var url = window.location.protocol + "//" + window.location.host + window.location.pathname;
+	var content = window.location.search.split("?")[1];
 	var pos = url.indexOf(".html");
 	var flag = url.indexOf("_zh");
-	if(window.location.pathname === '/') {
+	if (window.location.pathname === '/') {
 		newURL = window.location.protocol + "//" + window.location.host + '/index_zh.html';
 	} else if (pos > -1) {
-		if(flag > -1) {
-			newURL = url.substr(0, flag)+url.substr(pos);
+		if (flag > -1) {
+			newURL = url.substr(0, flag) + url.substr(pos);
 		} else {
-			newURL = url.substr(0, pos)+"_zh"+url.substr(pos);
+			newURL = url.substr(0, pos) + "_zh" + url.substr(pos);
 		}
 	}
-	window.location.href = newURL;
+	if (content !== undefined) {
+		window.location.href = newURL + "?" + content;
+	} else {
+		window.location.href = newURL;
+	}
 }
 
-function choose(choice){
-	sessionStorage.setItem("theContent", choice);
-}
